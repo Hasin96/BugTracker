469831de47118aed536666b1b5dcf310bd31fa3d
Hasin96 <Hasin.ajaz1996@hotmail.com>
2021-09-19 12:18:17 +0100

    Added initial iteration on creating a Task

--- BugTracker.UI.UnitTests/Project/TaskControllerTests/Create.cs
+++ BugTracker.UI.UnitTests/Project/TaskControllerTests/Create.cs
@@ -1,10 +1,53 @@
 ﻿using Xunit;
 using FluentAssertions;
+using BugTracker.Domain;
+using BugTracker.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Linq;
+using Moq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BugTracker.UI.UnitTests.TaskControllerTests
 {
     public class Create
     {
+        [Fact]
+        public async Task ShouldAddNewRequirementToProject()
+        {
+            int projectId = 1;
+            string requirement = "Add A FOOTER WHICH SHOWS THE STATUS";
+            var proj = new Project
+            {
+                Id = projectId,
+                Requirements = new List<Requirement>
+                {
+                    new Requirement
+                    {
+                        Description = requirement,
+                        Status = RequirementStatus.NotStarted,
+                        ProjectId = projectId
+                    }
+                }
+            };
+            var projectServiceResult = new ProjectServiceResult(proj, ProjectServiceCode.Success);
+            var projectSerivceMock = new Mock<IProjectService>();
+            projectSerivceMock.Setup(psm => psm.AddRequirementToProject(projectId, requirement))
+                .Returns(Task.FromResult(projectServiceResult));
+            
 
+            var taskController = new TasksApiController(projectSerivceMock.Object);
+
+            var result = await taskController.Create(projectId, requirement);
+            var r = result.As<OkObjectResult>();
+            r.StatusCode.Should().Be((int)HttpStatusCode.OK);
+
+            var project = r.Value.As<Project>();
+                          
+            project.Id.Should().Be(projectId);
+            project.Requirements.First().Description.Should().Be(requirement);
+        }
+        
     }
 }
--- BugTracker/Controllers/TasksApiController.cs
+++ BugTracker/Controllers/TasksApiController.cs
@@ -1,51 +1,58 @@
-﻿//using BugTracker.Models;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Task = BugTracker.Models.Task;
+﻿using BugTracker.Domain;
+using BugTracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace BugTracker.Controllers
-//{
-//    [Route("api/projects/{projectId}/tasks")]
-//    [ApiController]
-//    public class TasksApiController : ControllerBase
-//    {
-//        private BugTrackerContext _context;
+namespace BugTracker.Controllers
+{
+    [Route("api/projects/{projectId}/tasks")]
+    [ApiController]
+    public class TasksApiController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
 
-//        public TasksApiController(BugTrackerContext context)
-//        {
-//            _context = context;
-//        }
+        public TasksApiController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
 
-//        //[HttpPut("{taskId:int}")]
-//        //public async Task<IActionResult> Put(int projectId, int taskId, BugTracker.Domain.Project.Task newTask)
-//        //{
-//        //    try
-//        //    {
-//        //        var project = await _context.Projects.Include(proj => proj.Tasks).SingleOrDefaultAsync(proj => proj.Id == projectId);
-//        //        if (project is null) return NotFound($"Couldn't find the project with the project id of : ${projectId}");
+        public async Task<IActionResult> Create(int projectId, string requirement)
+        {
+            var response = await _projectService.AddRequirementToProject(projectId, requirement);
 
-//        //        if (taskId != newTask.Id)
-//        //            return BadRequest("task id does not match task object in request");
+            return Ok(response.Result);
+        }
 
-//        //        project.UpdateTaskStatus(newTask);
+        //[HttpPut("{taskId:int}")]
+        //public async Task<IActionResult> Put(int projectId, int taskId, BugTracker.Domain.Project.Task newTask)
+        //{
+        //    try
+        //    {
+        //        var project = await _context.Projects.Include(proj => proj.Tasks).SingleOrDefaultAsync(proj => proj.Id == projectId);
+        //        if (project is null) return NotFound($"Couldn't find the project with the project id of : ${projectId}");
 
-//        //        var task = project.Tasks.SingleOrDefault(t => t.Id == taskId);
-//        //        if (task is null) return NotFound($"Couldn't find the talk with the task id of : ${taskId} with the project id of : ${projectId}");
+        //        if (taskId != newTask.Id)
+        //            return BadRequest("task id does not match task object in request");
 
-//        //        await _context.SaveChangesAsync();
+        //        project.UpdateTaskStatus(newTask);
 
-//        //        return Ok(task);
-//        //    }
-//        //    catch (Exception ex)
-//        //    {
-//        //        return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure, failed to change tasks");
-//        //    }
-//        //}
-//    }
-//}
+        //        var task = project.Tasks.SingleOrDefault(t => t.Id == taskId);
+        //        if (task is null) return NotFound($"Couldn't find the talk with the task id of : ${taskId} with the project id of : ${projectId}");
+
+        //        await _context.SaveChangesAsync();
+
+        //        return Ok(task);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure, failed to change tasks");
+        //    }
+        //}
+    }
+}
